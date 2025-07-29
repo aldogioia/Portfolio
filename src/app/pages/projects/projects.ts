fb9357ea4a432b828d6944990379f2ae44680f41
aldogioia <aldogioia2002@gmail.com>
2025-07-29 22:52:08 +0200

    aggiunto nuovo progetto

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -13,6 +13,7 @@ export class Projects implements OnInit {
   projects: Project[] = [
     new Project("images/museoMultimedialeCosenza.png", "Museo Multimediale Cosenza", "https://museomultimedialecosenza.it", "July 2025"),
     new Project("images/tesi.png", "Bachelor's thesis", "https://github.com/aldogioia/Tesi", "April 2025"),
+    new Project("images/template_security_uml.png", "Security Template", "https://github.com/aldogioia/SecurityTemplate", "August 2025"),
   ];
 
   constructor(private titleService: Title, private metaService: Meta) {}
